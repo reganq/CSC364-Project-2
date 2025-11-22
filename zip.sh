d2b40ecb7669fefc5fd8c7de8b5601d585d380fd
Regan Quartus <regan.quartus@mail.utoronto.ca>
2025-11-21 21:50:46 -0500

    Complete Zipping Script

--- zip.sh
+++ zip.sh
@@ -2,15 +2,26 @@ cd /home/reganquartus/CSC364-Project-2
 mkdir project
 cd project
 
+mkdir p1
+mkdir p2
+
 # copy part 1
+cd p1
 mkdir artifacts
-cp -r ../p1/artifacts/* artifacts
+cp -r ../../p1/artifacts/* artifacts
 
 rm artifacts/fuzzing/fuzz_out
-rm artifacts/*/mini_proto.c
-
-# copy part 2
+rm artifacts/*/*.c
 
 cd ..
+
+# copy part 2
+cd p2
+mkdir artifacts
+cp -r ../../p2/artifacts/* artifacts
+
+rm artifacts/exploits/*/*.php
+
+cd ../..
 zip -r project2.zip project
 rm -rf project
